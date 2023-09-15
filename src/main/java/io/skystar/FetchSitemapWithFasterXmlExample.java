9d45c9044f78f8bddb881cf849cab38e01d2b708
Michael Tobolla <m.tobolla@t-online.de>
2023-09-15 14:53:17 +0200

    Implemented fasterXml

---
+++ src/main/java/io/skystar/FetchSitemapWithFasterXmlExample.java
@@ -0,0 +1,40 @@
+package io.skystar;
+
+import io.skystar.dto.PdfData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.List;
+
+public class FetchSitemapWithFasterXmlExample {
+
+    private static final Logger logger = LogManager.getRootLogger();
+
+    private static final String GET_URL = "https://www.arbeitsagentur.de/sitemap.xml";
+
+    public static void main(String[] args) {
+        FetchSitemapWithFasterXmlExample fetchSitemapWithFasterXMLExample = new FetchSitemapWithFasterXmlExample();
+        fetchSitemapWithFasterXMLExample.fetchWithFasterXml();
+    }
+
+    public void fetchWithFasterXml() {
+        try {
+            Document doc = Jsoup.connect(GET_URL)
+                    .maxBodySize(0)
+                    .get();
+
+            List<PdfData> pdfs = doc.getElementsByTag("url")
+                    .stream()
+                    .map(pdf -> new PdfData(pdf.getElementsByTag("loc").text()))
+                    .toList();
+
+            FileUtil.saveIntoFile(pdfs, "jackson.json");
+        } catch (IOException e) {
+            logger.error("Couldn't fetch URLs");
+        }
+    }
+
+}
---
+++ src/main/java/io/skystar/dto/PdfData.java
@@ -0,0 +1,10 @@
+package io.skystar.dto;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class PdfData {
+    private String url;
+}
