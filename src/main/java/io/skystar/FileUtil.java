f1a0fe13ff25db8321c388298a42c4a61e9409a1
Michael Tobolla <m.tobolla@t-online.de>
2023-09-15 14:46:56 +0200

    Implemented HTTPRequest

---
+++ src/main/java/io/skystar/FileUtil.java
@@ -0,0 +1,31 @@
+package io.skystar;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtil {
+
+    private static final Logger logger = LogManager.getRootLogger();
+
+    public static void saveIntoFile(Object input, String filename) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Path path = Path.of("out", filename);
+
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+
+            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(input);
+            Files.writeString(path, json);
+        } catch (IOException e) {
+            logger.error("Failed to save object into file");
+        }
+    }
+}
+
---
+++ src/main/java/io/skystar/SendHTTPExample.java
@@ -0,0 +1,51 @@
+package io.skystar;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class SendHTTPExample {
+
+    private static final Logger logger = LogManager.getRootLogger();
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+    private static final String GET_URL = "https://www.arbeitsagentur.de/sitemap.xml";
+
+    public static void main(String[] args) {
+        SendHTTPExample sendHTTPExample = new SendHTTPExample();
+        sendHTTPExample.fetchSitemap();
+    }
+
+    public void fetchSitemap() {
+        try {
+            URL url = new URL(GET_URL);
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+            logger.info("GET Response Code :: " + responseCode);
+            if (responseCode == HttpsURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                FileUtil.saveIntoFile(response, "pdfs.json");
+            } else {
+                logger.error("GET request did not work.");
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
+
