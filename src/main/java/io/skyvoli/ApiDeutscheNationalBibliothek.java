8cc8d3689719b1def6127ce7f1fa1ba6509b86fa
Michael Tobolla <m.tobolla@t-online.de>
2023-09-20 12:21:18 +0200

    Adjusted Serializing

--- src/main/java/io/skyvoli/ApiDeutscheNationalBibliothek.java
+++ src/main/java/io/skyvoli/ApiDeutscheNationalBibliothek.java
@@ -1,34 +1,41 @@
 package io.skyvoli;
 
-import io.skyvoli.dto.FullRecord;
-import io.skyvoli.dto.Record;
+import io.skyvoli.dto.Book;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ApiDeutscheNationalBibliothek {
 
     private static final Logger logger = LogManager.getRootLogger();
 
-    private static final String BASE_URL = "https://services.dnb.de/sru/dnb?version=1.1&operation=searchRetrieve";
+    private static final String BASE_URL = "https://services.dnb.de/sru/dnb?version=1.1";
+    private static final String OPERATION = "&operation=searchRetrieve";
+    private static final String RECORD_NUMBER = "&maximumRecords=100";
+    private static final String RECORD_SCHEMA = "&recordSchema=oai_dc";
 
     public static void main(String[] args) {
         new ApiDeutscheNationalBibliothek().start();
     }
 
     private void start() {
-        String url = this.buildUrl();
+        String searchQuery = "Love is war";
+        String url = this.buildUrl(searchQuery);
         Document xml = this.fetchXml(url);
         this.serializeXml(xml);
     }
 
-    private String buildUrl() {
+    private String buildUrl(String searchQuery) {
         //TODO Make query adjustable
-        return  BASE_URL + "&query=WOE%3D" + "Mika's%20Magic%20Market" + "&maximumRecords=100";
+        return BASE_URL + OPERATION + "&query=WOE%3D" + searchQuery + RECORD_NUMBER + RECORD_SCHEMA;
     }
 
     private Document fetchXml(String url) {
@@ -44,19 +51,25 @@ public class ApiDeutscheNationalBibliothek {
     }
 
     private void serializeXml(Document document) {
-        List<Record> records = document.getElementsByTag("record")
+
+        List<Elements> booksData = document.getElementsByTag("dc")
                 .stream()
-                .map(record -> new Record(record.getElementsByTag("dc:title").text(), record.getElementsByTag("dc:publisher").text()))
+                .map(Element::children)
                 .toList();
 
-        //TODO pretty printing into file
-        List<FullRecord> recordsComplete = document.getElementsByTag("record")
-                .stream()
-                //TODO Children Tag included
-                .map(element -> new FullRecord(element.children().eachText()))
-                .toList();
+        List<Book> serializedBooks = new ArrayList<>();
+        for (Elements bookData : booksData) {
+            HashMap<String, String> map = new HashMap<>();
+            for (Element data : bookData) {
+                if (map.containsKey(data.tagName())) {
+                    map.put(data.tagName() + data.attributes(), map.get(data.tagName()) + " + " + data.text());
+                } else {
+                    map.put(data.tagName() + data.attributes(), data.text());
+                }
+            }
+            serializedBooks.add(new Book(map));
+        }
 
-        FileUtil.saveIntoFile(records, "books.json");
-        FileUtil.saveIntoFile(recordsComplete, "fullRecord.json");
+        FileUtil.saveIntoFile(serializedBooks, "books.json");
     }
 }
---
+++ src/main/java/io/skyvoli/dto/Book.java
@@ -0,0 +1,24 @@
+package io.skyvoli.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.HashMap;
+
+@Getter
+@AllArgsConstructor
+public class Book {
+    private HashMap<String, String> data;
+    /*private String title;
+    private String creator;
+    private String publisher;
+    private String date;
+    private String language;
+    private String identifier;
+    private String subject;
+    private String rights;
+    private String description;
+    private String format;
+    private String type;
+    private String relation;*/
+}
