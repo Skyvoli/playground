1e7ef3aa93516f1a5e2b3bf8b5340618c550568e
Michael Tobolla <m.tobolla@t-online.de>
2023-09-22 09:39:09 +0200

    Get reduced Data

--- src/main/java/io/skyvoli/ApiDeutscheNationalBibliothek.java
+++ src/main/java/io/skyvoli/ApiDeutscheNationalBibliothek.java
@@ -16,26 +16,37 @@ import java.util.List;
 public class ApiDeutscheNationalBibliothek {
 
     private static final Logger logger = LogManager.getRootLogger();
-
     private static final String BASE_URL = "https://services.dnb.de/sru/dnb?version=1.1";
     private static final String OPERATION = "&operation=searchRetrieve";
-    private static final String RECORD_NUMBER = "&maximumRecords=100";
+    private static final String QUERY = "&query=";
+    private static final String MAXIMUM_RECORDS = "&maximumRecords=";
     private static final String RECORD_SCHEMA = "&recordSchema=oai_dc";
 
     public static void main(String[] args) {
-        new ApiDeutscheNationalBibliothek().start();
+        ApiDeutscheNationalBibliothek apiDeutscheNationalBibliothek = new ApiDeutscheNationalBibliothek();
+        apiDeutscheNationalBibliothek.start("Love is war", 100, "LoveIsWar.json");
+        apiDeutscheNationalBibliothek.start("Hell's paradise", 50, "HellsParadise.json");
+        apiDeutscheNationalBibliothek.getIsbn("Love is war", "LoveIsWarISBN.json");
+        apiDeutscheNationalBibliothek.getIsbn("Hell's paradise", "HellsParadiseISBN.json");
     }
 
-    private void start() {
-        String searchQuery = "Love is war";
-        String url = this.buildUrl(searchQuery);
+    private void start(String searchQuery, int recordNumber, String filename) {
+        String url = this.buildUrl(searchQuery, recordNumber);
         Document xml = this.fetchXml(url);
-        this.serializeXml(xml);
+        //TODO Parameter to decide which data should be added
+        List<Book> books = this.serializeXml(xml);
+        FileUtil.saveIntoFile(books, filename);
     }
 
-    private String buildUrl(String searchQuery) {
-        //TODO Make query adjustable
-        return BASE_URL + OPERATION + "&query=WOE%3D" + searchQuery + RECORD_NUMBER + RECORD_SCHEMA;
+    private void getIsbn(String searchQuery, String filename) {
+        String url = this.buildUrl(searchQuery, 10);
+        Document xml = this.fetchXml(url);
+        List<Book> books = this.serializeXml(xml);
+        FileUtil.saveIntoFile(this.reduceBookData(books), filename);
+    }
+
+    private String buildUrl(String searchQuery, int recordNumber) {
+        return BASE_URL + OPERATION + QUERY + searchQuery + MAXIMUM_RECORDS + recordNumber + RECORD_SCHEMA;
     }
 
     private Document fetchXml(String url) {
@@ -45,12 +56,12 @@ public class ApiDeutscheNationalBibliothek {
                     .maxBodySize(0)
                     .get();
         } catch (IOException e) {
-            logger.error("Couldn't fetch URLs");
+            logger.error("Couldn't fetch document");
         }
         return doc;
     }
 
-    private void serializeXml(Document document) {
+    private List<Book> serializeXml(Document document) {
 
         List<Elements> booksData = document.getElementsByTag("dc")
                 .stream()
@@ -61,6 +72,7 @@ public class ApiDeutscheNationalBibliothek {
         for (Elements bookData : booksData) {
             HashMap<String, String> map = new HashMap<>();
             for (Element data : bookData) {
+                data.removeAttr("xmlns:tel");
                 if (map.containsKey(data.tagName())) {
                     map.put(data.tagName() + data.attributes(), map.get(data.tagName()) + " + " + data.text());
                 } else {
@@ -69,7 +81,12 @@ public class ApiDeutscheNationalBibliothek {
             }
             serializedBooks.add(new Book(map));
         }
+        return serializedBooks;
+    }
 
-        FileUtil.saveIntoFile(serializedBooks, "books.json");
+    private List<Book> reduceBookData(List<Book> books) {
+        books.forEach(book -> book.getData().keySet()
+                .removeIf(key -> !(key.equals("dc:title") || key.equals("dc:identifier xsi:type=\"tel:ISBN\""))));
+        return books;
     }
 }
