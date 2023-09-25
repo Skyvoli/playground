69a05e865e156dc12ca303ae0ff536a60225776e
Michael Tobolla <m.tobolla@t-online.de>
2023-09-25 09:35:45 +0200

    Spring service implemented

--- src/main/java/io/skyvoli/springboot/Controller.java
+++ src/main/java/io/skyvoli/springboot/Controller.java
@@ -1,13 +1,17 @@
 package io.skyvoli.springboot;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class Controller {
+
+    private final SpringService springService;
 
     @GetMapping
     public String greeting() {
-        return "Hello World";
+       return springService.greeting();
     }
 }
---
+++ src/main/java/io/skyvoli/springboot/SpringService.java
@@ -0,0 +1,11 @@
+package io.skyvoli.springboot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringService {
+
+    public String greeting() {
+        return "Hello World";
+    }
+}
